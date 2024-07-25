ef625314196b4dce9cf47e709fc55c132eb8e1b9
Omer Catalbas <78023319+omrctlbs@users.noreply.github.com>
2024-07-25 20:40:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Variables_and_Data_Types;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int thisyear = DateTime.Now.Year;
+       int age;
+       int birthyear;
+
+       Console.WriteLine("Please enter your birthyear");
+       birthyear = Convert.ToInt32(Console.ReadLine());
+       age = thisyear - birthyear;
+       Console.WriteLine("Your age: " + age);
+
+       if (age <= 18) {
+        Console.WriteLine("You are not of legal age");
+       }
+       else{
+        Console.WriteLine("You are legal age");
+       }
+    }
+}
